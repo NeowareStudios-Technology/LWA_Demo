3817683518a9546d017e38e7bd32bfa697f09cc6
Alex <chatham.alexander@gmail.com>
2018-08-24 12:33:46 -0400

    added activity panel

---
+++ LWA Demo/Assets/changeRotationSpeed.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeRotationSpeed : MonoBehaviour {
+
+    public GameObject rotor;
+    public Text speedText;
+    public Text powered;
+    public Text energy;
+    public void changeSpeed()
+    {
+        int i = Mathf.FloorToInt(GetComponent<Slider>().value);
+        rotor.GetComponent<loadingRotation>().speed = i;
+        speedText.text = (i/6).ToString() + " mph";
+        if(i> 250){
+            powered.text = "City";
+        }else{
+            if(i > 100){
+                powered.text = "Street";
+            } else {
+                if(i > 50){
+                    powered.text = "House"; 
+                }else{
+                    if(i == 0){
+                        powered.text = "Nothing";
+                    }
+                }
+            }
+        }
+
+        energy.text = (.0006 * (i/12) * (i / 12) * (i / 12) * 1000 * 81 * .1).ToString() + " KWH";
+
+    }
+}
---
+++ LWA Demo/Assets/loadingRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadingRotation : MonoBehaviour {
+
+    public Transform transform;
+    // Update is called once per frame
+    public bool right = true;
+    public float z;
+    public int speed = 10;
+	void Update () {
+        if (right)
+            z += Time.deltaTime * speed;
+        else
+            z -= Time.deltaTime * speed;
+
+        transform.rotation = Quaternion.Euler(0, 0, z);
+    }
+}
